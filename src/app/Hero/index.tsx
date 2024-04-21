e38c6a2aeb729808ff2d457c9eac1cb2c36d594a
Eric Chung <rchung.eric@gmail.com>
2024-04-21 21:25:24 +1000

    Setup

---
+++ src/app/Hero/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles.module.scss';
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>Hero</div>
+  )
+}
+
+export default Hero
